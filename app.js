939d082219a1a6591e54bb78665d9fd2674b8802
Aswin <aswindilip5@gmail.com>
2022-07-28 12:01:40 +0530

    temperature set

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const request = require('request');
 const app = express();
+require('dotenv').config();
+const key = process.env.API_KEY
+let place ;
+let temp ;
+let feels_like;
+let min;
+let max;
+let desc;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine",'ejs');
+app.set("view engine", 'ejs');
 
-app.get("/",(req,res)=>{
-    res.render("index",)
+
+
+app.get("/", (req, res) => {
+
+    res.render("index", { CITY: place, TEMP: temp,FEELS:feels_like,MAX:max ,MIN:min,DESCRIPTION:desc });
+    
 });
-app.listen(3000,()=>{
+
+app.post("/", (req, resp) => {
+    place = req.body.city;
+     let url = `http://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=${key}`
+    request(url, (err, res, body) => {
+    if (err) {
+        console.log(err)
+    }else{
+        let weather = JSON.parse(body)
+          temp = weather.main.temp;
+          feels_like = weather.main.feels_like;
+          min = weather.main.temp_min;
+          max = weather.main.temp_max;
+          desc =weather.weather[0].description;
+          resp.redirect("/");
+    }
+});
+   
+});
+
+app.listen(3000, () => {
     console.log("server running in port 3000");
 })
