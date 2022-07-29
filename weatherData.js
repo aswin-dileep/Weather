41ceda0e9adb8441fa044c85eb715e09891f6e8c
Aswin <aswindilip5@gmail.com>
2022-07-29 17:53:08 +0530

    weatherData module deleted

--- weatherData.js
+++
@@ -1,9 +0,0 @@
-const request = require('request');
-const key = "654c302bce3c08d025434466eee4cf8f"
-request('https://api.openweathermap.org/data/2.5/weather?q={"place"}&appid=key',(err,req,res)=>{
- if(err){
-    console.log(err)
- }
- console.log(body.url);
- console.log(body.explanation)
-});
